2a58410ecfabf7eb38bfcc43ee7f4548ba1826da
Madhav Ghei <madhav.ghei@gmail.com>
2018-08-31 14:52:14 -0500

    last commit as a REU! commented index.html, run_model_text.py and app.py heavily. added issues to github for future expansion
    Former-commit-id: 2b0894140ba8a48a7cb85a283f11d6bff492f23d [formerly 183bd7366452d04dfa4726b9be0f84db6f652627]
    Former-commit-id: 46320f9c2e0a09cc6b524d518b42232d6bc9faed
    Former-commit-id: bcbdcdbacd94c9ff9767c713fb23d484cfaae1b3

--- Recording/FlaskApp/app.py
+++ Recording/FlaskApp/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask.json import jsonify
-import os, keras, librosa, json
+import os, keras, librosa, json, argparse
 from werkzeug import secure_filename
 
 from run_model_text import *
@@ -10,21 +10,51 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT,'uploads')
 
 app = Flask(__name__)
- 
+
+def str2bool(v): # code to accept multiple alternatives for True/False (from stackoverflow)
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser() # set up parser to grab optional inputs: -m specifies model path, -d specifies debug mode on/off, -t specifies threading on/off, -db specifies sound database to search
+parser.add_argument("-m", "--model", help="The filepath of the model you wish to use.", type=str, default="./model/model_11-10_top_pair.h5")
+parser.add_argument("-d", "--debug", help="Sets debug=\"true\" if \"True\", false otherwise.", type=str2bool, default=False)
+parser.add_argument("-t", "--threaded", help="Sets threaded=\"true\" if \"True\", false otherwise.", type=str2bool, default=False)
+parser.add_argument("-db", "--database", help="The filepath of the database containing audio you wish to use as the search space", type=str, default="./static/")
+
+
+
+args = parser.parse_args()
+
+model_path = args.model
+model = load_model(model_path)
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+	filename_list = []
+	f_list = os.listdir(args.database)
+	for f in f_list:
+		if ".wav" in f.lower() or ".mp3" in f.lower(): # grab all filenames of audio files in search database, for autocomplete
+			filename_list.append(f[:-4]) 
+	
+	return render_template('index.html', filenamelist=filename_list)
 
 @app.route('/search', methods = ['GET', 'POST'])
 def search():
+	
+	# retrieve FormData from the AJAX request
 	if request.method == 'POST':
-		file = request.files['file']
-		offset = request.form['start']
-		duration = request.form['length']
-		text = request.form['textDescription']
+		file = request.files['file'] # blob of audio file of the user recording
+		offset = request.form['start'] # time offset of starting point of imitation (used to trim audio file)
+		duration = request.form['length'] # time offset of ending point of imitation (used to trim audio file)
+		text = request.form['textDescription'] # string containing text filter (optional)
 		print text
 		if file:
-			# save incoming audio file
+			# save incoming blob as .wav audio file
 			filename = secure_filename(file.filename) + '.wav'
 			file.save(os.path.join(UPLOAD_FOLDER, filename))
 
@@ -43,18 +73,17 @@ def search():
 			# define filepaths for the keras model
 			imi_path = outgoing_filepath
 			ref_dir = './static/'
-			model_path = './model/model_11-10_top_pair.h5'
 
-			# run the siamese network
-			sorted_filenames, sorted_filenames_matched = search_audio(imi_path, ref_dir, model_path, text)
+			# run the siamese network, giving it our recorded imitation, the directory of audio files it is to search, the path to the keras model, and the text query
+			sorted_filenames, sorted_filenames_matched = search_audio(imi_path, ref_dir, model, text)
 
-			#return the results to a Python list
+			#return the results to a Python list, delimit the matched vs unmatched lists with a string '...', this will be parsed on the front-end
 			results = sorted_filenames.tolist() + ['...'] + sorted_filenames_matched.tolist()
 			print results
 
-	# can't send a list with Flask so we'll make it a long string and parse on the front end
+	# can't send a list with Flask so we'll make it a long string and deal with it on the front-end
 	return ','.join(results)
 
  
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=args.debug, threaded=args.threaded)
--- Recording/FlaskApp/run_model_text.py
+++ Recording/FlaskApp/run_model_text.py
@@ -4,12 +4,14 @@ import numpy as np
 import librosa
 from keras.models import load_model
 from keras import backend as K
+import pickle 
 #K.set_image_dim_ordering('th')
 #os.environ["CUDA_VISIBLE_DEVICES"]="1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 np.random.seed(1337)
 
+old_ref_dir = './static/'
 
 def normalize_spectrogram(data):
     '''
@@ -89,41 +91,66 @@ def preprocessing_imi(imi_path):
 
     return imi_spectrogram_norm
 
+def get_old_ref_dir(ref_dir):
+    try:
+        with open('ref_dir.pickle', 'rb') as handle:
+            old_ref_dir = pickle.load(handle)
+    except EnvironmentError:
+        with open('ref_dir.pickle', 'w+b') as handle:
+            old_ref_dir = ref_dir
+            pickle.dump(old_ref_dir, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print "OLD_REF_DIR: ", old_ref_dir
+    return old_ref_dir
 
-def search_audio(imi_path, ref_dir, model_path, text):
+def update_old_ref_dir(old_ref_dir):
+    with open('ref_dir.pickle', 'wb') as handle:
+        pickle.dump(old_ref_dir, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def search_audio(imi_path, ref_dir, model, text):
     
-    # Keras model     
-    model = load_model(model_path)
-
     # imitation query
     imi_data = preprocessing_imi(imi_path)
 
+    # check if given ref_dir was used last time. if it was, then use the saved list instead of re-preprocessing all ref files
+    old_ref_dir = get_old_ref_dir(ref_dir)
 
-    # To speed up, the spectrogram of reference recordings are saved.
-    # If a new set of reference recordings are added, you should run 'preprecessing_ref' function 
-    # ref_filenames, ref_data = preprocessing_ref(ref_dir)
-    # np.save('./preprocessed_data/ref_filenames.npy', ref_filenames)
-    # np.save('./preprocessed_data/ref_data.npy', ref_data)
+    if ref_dir != old_ref_dir: # we need to preprocess the reference audio files if we haven't done that already
+        print "REF_DIR updated. Preprocessing new reference data..."
+        ref_filenames, ref_data = preprocessing_ref(ref_dir)
+        np.save('./preprocessed_data/ref_filenames.npy', ref_filenames)
+        np.save('./preprocessed_data/ref_data.npy', ref_data)
+
+    old_ref_dir = ref_dir # save the current reference directory to check the next time we run the program
+    update_old_ref_dir(old_ref_dir)
+
     old_ref_filenames = np.load('./preprocessed_data/ref_filenames.npy')
     old_ref_data = np.load('./preprocessed_data/ref_data.npy')
 
-    i_delete = []
-    text_match_data = []
-    text_match_filenames = []
-    for i, fn in enumerate(old_ref_filenames):
-        if text in fn.lower():
+    i_delete = [] # contains indices of text-matched filenames (same indexing as spectrograms, so we can use these indices to remove those files from both the filenames and spectrgrams arrays)
+    text_match_data = [] # will contain spectrograms of files who matched the text query
+    text_match_filenames = [] # will contain filenames that matched the text query
+
+    for i, fn in enumerate(old_ref_filenames): # gets all matches of text filenames (without extension) within our database and puts them in a list
+        if text.lower() in fn[:-4].lower():
             i_delete.append(i)
             text_match_filenames.append(fn)
             text_match_data.append(old_ref_data[i])
 
-    ref_filenames = np.delete(old_ref_filenames, i_delete,0)
-    ref_data = np.delete(old_ref_data, i_delete,0)
-    sorted_filenames = inference(ref_data, ref_filenames, imi_data, model)
+    ref_filenames = np.delete(old_ref_filenames, i_delete,0) #remove the text matches from the original list of filenames
+    ref_data = np.delete(old_ref_data, i_delete,0) #remove the matches from the original list of spectrograms
+    sorted_filenames = inference(ref_data, ref_filenames, imi_data, model) #run the model on the non-matched files (i.e., sort them by similarity to imitation)
 
-    if len(text_match_filenames) != 0:
+    if len(text_match_filenames) != 0: #if there was a text query supplied, run inference on the text matched files
         sorted_filenames_matched = inference(np.array(text_match_data), np.array(text_match_filenames), imi_data, model)
     else: 
-        sorted_filenames_matched = np.array([])
+        sorted_filenames_matched = np.array([]) # if no text was supplied, there will be no matches, so return an empty array
+
+    if text =='':
+        temp = []
+        temp = sorted_filenames_matched
+        sorted_filenames_matched = sorted_filenames
+        sorted_filenames = temp
 
     return sorted_filenames, sorted_filenames_matched
     
@@ -132,7 +159,7 @@ def inference(ref_data, ref_filenames, imi_data, model):
     # create pairs of input to Siamese network
     pairs_imi_left = []
     pairs_ref_right = []
-    for ref in ref_data:
+    for ref in ref_data: # create an [imitation, reference] pair to go to the model for inference
         pairs_imi_left.append(imi_data[0])
         pairs_ref_right.append(ref)
 
@@ -146,9 +173,11 @@ def inference(ref_data, ref_filenames, imi_data, model):
     model_output = model.predict([pairs_imi_left, pairs_ref_right],  batch_size=1, verbose=1)
     model_output = np.array(model_output)
 
+
+    # sort the reference recordings by the predicted similarities between the imitation and all the reference recodrings
     sorted_index = np.argsort(model_output.flatten())[::-1]
     sorted_filenames = ref_filenames[sorted_index]   
-    # sort the reference recordings by the predicted similarities between the imitaiton and all the reference recodrings
+    
     
     return sorted_filenames
 
